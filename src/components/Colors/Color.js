f65852e4de7c334c4ccf59da85e290aaecacee0b
Connor McCue <connor.mccue@comcast.net>
2022-09-27 15:39:33 -0700

    commit early commit often

---
+++ src/components/Colors/Color.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Color.css';
+
+export default function Color() {
+  return (
+    <div className="color">Color</div>
+  );
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <>
-      <div><Link to={'/red'}> Click here! </Link></div>
-      <div><Link to={'/green'}> Click here! </Link></div>
-      <div><Link to={'/blue'}> Click here! </Link></div>
+      <div><Link to={'/List'}> Click here! </Link></div>
     </>
   );
 }
