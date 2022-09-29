e3165e4f100ce98c581b46177f124463a0ccf2e5
Connor McCue <connor.mccue@comcast.net>
2022-09-28 18:20:37 -0700

    attempt 2: added header, updated app home and index, bare bones working product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Colors!</h1>
-      <Switch>
-        <Route exact path="/rgb" component={Home} />
-      </Switch>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path={`/rgb/:red/:green/:blue`} component={Home} ></Route>
+        </Switch>
+      </div>
     </>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import './Home.css';
 
 export default function Home() {
   const id = useParams();
