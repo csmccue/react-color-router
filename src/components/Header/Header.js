283b3767612d2cb4518aaac700700c9195bc47ac
Connor McCue <connor.mccue@comcast.net>
2022-09-28 18:56:52 -0700

    touch ups

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,9 @@ export default function Header() {
     <>
       <header>
         <ul className='links'>
-          <NavLink className='green' activeStyle={{ color: `rgb(0,132,87)` }} to="/rgb/0/132/87">Green</NavLink>
-          <NavLink className='red' activeStyle={{ color: `rgb(237,27,45)` }} to="/rgb/237/27/45">Red</NavLink>
-          <NavLink className='white' activeStyle={{ color: `rgb(238,238,238)` }} to="/rgb/238/238/238">White</NavLink>
+          <NavLink className='green' activeStyle={{ color: `rgb(0,132,87)` }} to="/rgb/0/132/87">Chilis Green</NavLink>
+          <NavLink className='red' activeStyle={{ color: `rgb(237,27,45)` }} to="/rgb/237/27/45">Chilis Red</NavLink>
+          <NavLink className='white' activeStyle={{ color: `rgb(238,238,238)` }} to="/rgb/238/238/238">Chilis White</NavLink>
         </ul>
       </header>
     </>
