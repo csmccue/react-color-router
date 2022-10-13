b2c69980d1757a96795690f5c3f7dd3484ccaf89
Connor McCue <connor.mccue@comcast.net>
2022-10-13 15:18:03 -0700

    generic not found component loads when path is not correct color router rgb path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path={`/rgb/:red/:green/:blue`} component={Home} ></Route>
+          <Route path={`*`} component={NotFound} ></Route>
         </Switch>
       </div>
     </>
